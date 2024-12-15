27212fa1f495a1a81ba2bbb436d7e5c9b0a14bbc
Ada Qi <97003885+z3qi@users.noreply.github.com>
2024-12-15 13:03:05 -0800

    Create ProCtrl_revised.sv

---
+++ ProCtrl_revised.sv
@@ -0,0 +1,62 @@
+module ProgCtr(
+  input             Clk,
+                    Reset,
+  					start,
+                    Jen,
+  input  logic[7:0] Jump,
+  input  logic[7:0] PC_prev,
+  input  logic[1:0] problem,
+
+  output      [8:0] Instruction,
+  output logic[7:0] PC,
+  output logic       pc_done_flag
+);
+
+  int i;
+  // InstROM
+  InstROM instr_rom(
+    .PC(PC),
+    .mach_code(Instruction)
+  );
+
+  // Update PC
+
+  // 这里需要修改，PC需要一个来自control的input，来决定是否需要更新PC
+  // always_ff @(posedge Clk) begin
+  //   if(Reset)
+  //     PC <= 'b0;
+  //   else if(Jen)
+  //     PC <= Jump;
+  //   else begin
+  //     PC <= PC_prev + 6'd1;
+      
+  //     if(PC == 6'd256) begin
+  //       pc_done_flag <= 1'b1;
+  //     end
+  //   end
+  // end
+
+  always_ff @(posedge Clk or posedge Reset or posedge start) begin
+    if (Reset || start) begin
+    PC <= -1;
+    pc_done_flag <= 1'b0;
+  end else begin
+    if (Jen) begin
+      PC <= Jump;
+//      $display("Jump to line %d", Jump);
+    end else begin
+      PC <= PC_prev + 8'd1;
+//      $display("Continue to line %d", PC);
+      if (problem == 2'b00 && PC == 8'd118)
+        pc_done_flag <= 1'b1;
+      else if (problem == 2'b01 && PC == 8'd105)
+        pc_done_flag <= 1'b1;
+//      else if (problem == 2'b10 && PC == 8'd88)
+      else if (problem == 2'b10 && PC == 8'd81)
+        pc_done_flag <= 1'b1;
+    end
+  end
+end
+
+
+endmodule
